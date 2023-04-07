7e5e2de234d9fcbaf280a92b44f3da1df95e4696
Lautaro Demonte <lauty.d.p@gmail.com>
2023-04-07 16:29:31 -0300

    cambio de nombre a la carpeta mongo y avanzo con sus managers

---
+++ src/dao/mongo/cartManagerMongo.js
@@ -0,0 +1,32 @@
+import { cartsModel } from "../models/carts.model.js";
+
+export default class CartManager {
+  async createCart() {
+    try {
+      const cart = await cartsModel.create();
+      return cart;
+    } catch (error) {
+      console.log(`Error creando carrito: ${error.message}`);
+    }
+  }
+
+  async getById(id) {
+    try {
+      const cart = await cartsModel.findOne({ _id: id }).populate("products");
+      return cart;
+    } catch (error) {
+      console.log(
+        `Error buscando el carrito con el id ${id}: ${error.message}`
+      );
+    }
+  }
+
+  async addToCart(cid, pid) {
+    try {
+      const cart = await this.getById(cid);
+      const carts = await cartsModel.updateOne({ _id: cid }, { $set: {} });
+    } catch (error) {
+      console.log(`Error agregando producto al carrito: ${error.message}`);
+    }
+  }
+}
